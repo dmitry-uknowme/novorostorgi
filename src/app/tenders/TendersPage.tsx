d2e4a0fd3b3b476d130b5136e2ff756b0bab4760
you@example.com <you@example.com>
2023-03-07 16:39:27 +0500

    add tenders page

--- src/app/tenders/TendersPage.tsx
+++ src/app/tenders/TendersPage.tsx
@@ -3,28 +3,31 @@ import TenderCard, { ITenderCard } from "./component/card/Card";
 
 const tenders: ITenderCard[] = [
   {
-    customer_name: "test",
+    customer_name: 'АКЦИОНЕРНОЕ ОБЩЕСТВО "КОРПОРАЦИЯ РАЗВИТИЯ"',
     method: "method",
     method_localized: "Конкурентный отбор",
     number: 150,
     price: "price",
-    price_localized: "price",
+    price_localized: "2 600,89",
     registry_number: "3214214124",
-    review_date: "111",
+    date_review: "2023 08-09-2022",
     section: "section",
     section_localized: "223-ФЗ",
     status: "STATUS",
-    title: "Поставка котла для варки пива",
+    status_localized: "Идет прием заявок",
+    title: "Поставка рака для варки пива",
   },
 ];
 
 const TendersPage = () => {
   return (
     <MainTemplate>
-      <div className="col-md-8 offset-md-2">
-        {tenders.map((tender) => (
-          <TenderCard tender={tender} />
-        ))}
+      <div className="container">
+        <div className="col-md-8">
+          {tenders.map((tender) => (
+            <TenderCard tender={tender} />
+          ))}
+        </div>
       </div>
     </MainTemplate>
   );
--- src/app/tenders/component/card/Card.tsx
+++ src/app/tenders/component/card/Card.tsx
@@ -1,3 +1,5 @@
+import Button from "../../../component/button/Button";
+import ButtonPrimary from "../../../component/button/primary/ButtonPrimary";
 import styles from "./card.module.sass";
 
 export interface ITenderCard {
@@ -5,6 +7,7 @@ export interface ITenderCard {
   registry_number: string;
   title: string;
   status: string;
+  status_localized: string;
   customer_name: string;
   //TODO:
   method: string;
@@ -14,7 +17,7 @@ export interface ITenderCard {
   section_localized: string;
   price: string;
   price_localized: string;
-  review_date: string;
+  date_review: string;
 }
 
 interface TenderCardProps {
@@ -24,48 +27,78 @@ interface TenderCardProps {
 const TenderCard: React.FC<TenderCardProps> = ({ tender }) => {
   const {} = tender;
   return (
-    <div className="_Card">
+    <div className={styles._Card}>
       <div className="row">
-        <div className="col-md-7">
-          <div className="main">
-            <div className="col-md-5">
-              <div className="number">
-                № {tender.number}
-                {tender.registry_number ? ` / ${tender.registry_number}` : null}
-              </div>
-              <div className={`title ${styles.row}`}>
-                <div className="row__key">Наименование</div>
-                <div className="row__value">{tender.title}</div>
-              </div>
-              <div className={`${styles.row} customer`}>
-                <div className="row__key">Заказчик</div>
-                <div className="row__value">
-                  АКЦИОНЕРНОЕ ОБЩЕСТВО "КОРПОРАЦИЯ РАЗВИТИЯ"
+        <div className="col-md-8">
+          <div className={styles.main}>
+            <div className="row mb-1">
+              <div className="col-md-5">
+                <div className={styles.number}>
+                  № {tender.number}
+                  {tender.registry_number
+                    ? ` / ${tender.registry_number}`
+                    : null}
                 </div>
               </div>
-              <div className="row">
-                <div className="col-md-7">
-                  <div className={styles.row}>
-                    <div className="row__key">Секция:</div>
-                    <div className="row__value">{tender.section_localized}</div>
+              <div className="col-md-7">
+                <div className={`${styles.row} ${styles.status}`}>
+                  {/* <div className={styles.row__key}>Статус:&nbsp;</div> */}
+                  <div className={`${styles.status_circle}`}></div>
+                  &nbsp;&nbsp;
+                  <div className={styles.row__value}>
+                    {tender.status_localized}
                   </div>
                 </div>
-                <div className="col-md-5">
-                  <div className={styles.row}>
-                    <div className="row__key">Способ проведения:</div>
-                    <div className="row__value">{tender.method_localized}</div>
+              </div>
+            </div>
+            <div className={`${styles.row} ${styles.title}`}>
+              <div className={styles.row__key}>Наименование</div>
+              <div className={styles.row__value}>{tender.title}</div>
+            </div>
+            <div className={`${styles.row} ${styles.customer}`}>
+              <div className={styles.row__key}>Заказчик</div>
+              <div className={styles.row__value}>{tender.customer_name}</div>
+            </div>
+            <div className="row mt-1">
+              <div className="col-md-5">
+                <div className={`${styles.row} ${styles.section}`}>
+                  <div className={styles.row__key}>Секция:&nbsp;</div>
+                  <div className={styles.row__value}>
+                    {tender.section_localized}
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-7">
+                <div className={`${styles.row} ${styles.method}`}>
+                  <div className={styles.row__key}>
+                    Способ проведения:&nbsp;
+                  </div>
+                  <div className={styles.row__value}>
+                    {tender.method_localized}
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="col-md-5">
+        <div className="col-md-4">
           <div className="right">
-            <div className={styles.row}>
-              <div className="row__key">Начальная цена</div>
-              <div className="row__value"></div>
+            <div className={`${styles.row} ${styles.price}`}>
+              <div className={styles.row__key}>Начальная цена</div>
+              <div className={styles.row__value}>{tender.price_localized}</div>
             </div>
+            <div className={`${styles.row} ${styles.date_review} mt-1 mb-1`}>
+              <div className={styles.row__key}>Дата окончания подачи:</div>
+              <div className={styles.row__value}>{tender.date_review}</div>
+            </div>
+            <div className={`${styles.row} ${styles.timer}`}>
+              <div className={styles.row__key}>До окончания:&nbsp;</div>
+              <div className={styles.row__value}>
+                01:52:03
+                {/* {tender.date_review} */}
+              </div>
+            </div>
+            <ButtonPrimary className={styles.btn} title="Перейти" />
           </div>
         </div>
       </div>
